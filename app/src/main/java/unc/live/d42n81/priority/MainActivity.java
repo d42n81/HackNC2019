5d6ed65e8581a506e9cc737fb076d11b00e9f2bc
David Moore <dmoore42n81@gmail.com>
2019-10-12 12:12:20 -0400

    Initial commit

---
+++ app/src/main/java/unc/live/d42n81/priority/MainActivity.java
@@ -0,0 +1,14 @@
+package unc.live.d42n81.priority;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
